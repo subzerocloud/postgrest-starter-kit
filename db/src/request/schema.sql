2f5d1eec957c1a9618bb86a2f7d04f2d200e371a
Ruslan Talpa <ruslan.talpa@gmail.com>
2017-05-26 10:25:00 +0300

    simplify env_var function

--- db/src/request/schema.sql
+++ db/src/request/schema.sql
@@ -3,18 +3,8 @@ drop schema if exists request cascade;
 create schema request;
 
 create or replace function request.env_var(v text) returns text as $$
-  declare
-    result text;
-  begin
-    begin
-      select current_setting(v) into result;
-    exception 
-      when undefined_object then
-        return null;
-    end;
-    return result;
-  end;
-$$ stable language plpgsql;
+    select current_setting(v, true);
+$$ stable language sql;
 
 create or replace function request.jwt_claim(c text) returns text as $$
     select request.env_var('request.jwt.claim.' || c);
