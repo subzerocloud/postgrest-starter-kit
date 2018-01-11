1144e8818a7cdd0b1f1bb48ebfa27624ea9c7284
Ruslan Talpa <ruslan.talpa@gmail.com>
2018-01-11 10:15:44 +0200

    better rabbitmq.on_row_changer trigger * ability to decide what row columns to include/exclude * output forwarded messages in pg_amqp_bridge

--- db/src/data/todo.sql
+++ db/src/data/todo.sql
@@ -5,6 +5,13 @@ create table todo (
 	owner_id int references "user"(id) default request.user_id()
 );
 
+-- attach the trigger to send events to rabbitmq
+-- there is a 8000 bytes hard limit on the message payload size (PG NOTIFY) so it's better not to send data that is not used
+-- on_row_change call can take the following forms
+-- on_row_change() - send all columns
+-- on_row_change('{"include":["id"]}'::json) - send only the listed columns
+-- on_row_change('{"exclude":["bigcolumn"]}'::json) - exclude listed columns from the payload
+
 create trigger send_change_event
 after insert or update or delete on todo
-for each row execute procedure rabbitmq.on_row_change();
+for each row execute procedure rabbitmq.on_row_change('{"include":["id","todo"]}');
--- db/src/libs/rabbitmq/schema.sql
+++ db/src/libs/rabbitmq/schema.sql
@@ -16,19 +16,58 @@ $$ stable language sql;
 create or replace function rabbitmq.on_row_change() returns trigger as $$
   declare
     routing_key text;
-    row record;
+    row jsonb;
+    config jsonb;
+    excluded_columns text[];
+    col text;
   begin
     routing_key := 'row_change'
                    '.table-'::text || TG_TABLE_NAME::text || 
                    '.event-'::text || TG_OP::text;
     if (TG_OP = 'DELETE') then
-        row := old;
+        row := row_to_json(old)::jsonb;
     elsif (TG_OP = 'UPDATE') then
-        row := new;
+        row := row_to_json(new)::jsonb;
     elsif (TG_OP = 'INSERT') then
-        row := new;
+        row := row_to_json(new)::jsonb;
     end if;
-    perform rabbitmq.send_message('events', routing_key, row_to_json(row)::text);
+
+    -- decide what row columns to send based on the config parameter
+    -- there is a 8000 byte hard limit on the payload size so sending many big columns is not possible
+    if ( TG_NARGS = 1 ) then
+      config := TG_ARGV[0];
+      if (config ? 'include') then
+          --excluded_columns := ARRAY(SELECT unnest(jsonb_object_keys(row)::text[]) EXCEPT SELECT unnest( array(select jsonb_array_elements_text(config->'include')) ));
+          -- this is a diff between two arrays
+          excluded_columns := array(
+            -- array of all row columns
+            select unnest(
+              array(select jsonb_object_keys(row))
+            ) 
+            except
+            -- array of included columns
+            select unnest(
+              array(select jsonb_array_elements_text(config->'include'))
+            )
+          );
+      end if;
+
+      if (config ? 'exclude') then
+        excluded_columns := array(select jsonb_array_elements_text(config->'exclude'));
+      end if;
+
+      if (current_setting('server_version_num')::int >= 100000) then
+          row := row - excluded_columns;
+      else
+          FOREACH col IN ARRAY excluded_columns
+          LOOP
+            row := row - col;
+          END LOOP;
+      end if;
+    end if;
+    
+    perform rabbitmq.send_message('events', routing_key, row::text);
     return null;
   end;
 $$ stable language plpgsql;
+
