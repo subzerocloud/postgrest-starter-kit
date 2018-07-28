e524b7b7ccc880f2070fee318b3b16b7bbf378be
Philipp Hotz <p.hotz@posteo.net>
2018-07-28 20:24:26 +0200

    Run standard --fix
    https://www.npmjs.com/package/standard

--- tests/rest/auth.js
+++ tests/rest/auth.js
@@ -1,27 +1,27 @@
 import { rest_service, resetdb } from './common'
 import { should } from 'should'
 
-describe('auth', function() {
-  before(function(done){ resetdb(); done(); });
-  after(function(done){ resetdb(); done(); });
-  
-  it('login', function(done) {
+describe('auth', function () {
+  before(function (done) { resetdb(); done() })
+  after(function (done) { resetdb(); done() })
+
+  it('login', function (done) {
     rest_service()
       .post('/rpc/login?select=me,token')
       .set('Accept', 'application/vnd.pgrst.object+json')
-      .send({ 
-        email:"alice@email.com",
-        password: "pass"
+      .send({
+        email: 'alice@email.com',
+        password: 'pass'
       })
       .expect('Content-Type', /json/)
       .expect(200, done)
-      .expect( r => {
-        //console.log(r.body)
-        r.body.me.email.should.equal('alice@email.com');
+      .expect(r => {
+        // console.log(r.body)
+        r.body.me.email.should.equal('alice@email.com')
       })
-  });
+  })
 
-  it('me', function(done) {
+  it('me', function (done) {
     rest_service()
       .post('/rpc/me')
       .set('Accept', 'application/vnd.pgrst.object+json')
@@ -29,13 +29,13 @@ describe('auth', function() {
       .send({})
       .expect('Content-Type', /json/)
       .expect(200, done)
-      .expect( r => {
-        //console.log(r.body)
-        r.body.email.should.equal('alice@email.com');
+      .expect(r => {
+        // console.log(r.body)
+        r.body.email.should.equal('alice@email.com')
       })
-  });
+  })
 
-  it('refresh_token', function(done) {
+  it('refresh_token', function (done) {
     rest_service()
       .post('/rpc/refresh_token')
       .set('Accept', 'application/vnd.pgrst.object+json')
@@ -43,27 +43,26 @@ describe('auth', function() {
       .send({})
       .expect('Content-Type', /json/)
       .expect(200, done)
-      .expect( r => {
-        //console.log(r.body)
-        r.body.length.should.above(0);
+      .expect(r => {
+        // console.log(r.body)
+        r.body.length.should.above(0)
       })
-  });
+  })
 
-  it('signup', function(done) {
+  it('signup', function (done) {
     rest_service()
       .post('/rpc/signup')
       .set('Accept', 'application/vnd.pgrst.object+json')
-      .send({ 
-        name: "John Doe",
-        email:"john@email.com",
-        password: "pass"
+      .send({
+        name: 'John Doe',
+        email: 'john@email.com',
+        password: 'pass'
       })
       .expect('Content-Type', /json/)
       .expect(200, done)
-      .expect( r => {
-        //console.log(r.body)
-        r.body.me.email.should.equal('john@email.com');
+      .expect(r => {
+        // console.log(r.body)
+        r.body.me.email.should.equal('john@email.com')
       })
-  });
-
-});
+  })
+})
--- tests/rest/common.js
+++ tests/rest/common.js
@@ -1,10 +1,41 @@
-import jsonwebtoken from 'jsonwebtoken';
-import request from 'supertest';
-import {config} from 'dotenv';
-import {spawnSync} from 'child_process';
+import jsonwebtoken from 'jsonwebtoken'
+import request from 'supertest'
+import { config } from 'dotenv'
+import { spawnSync } from 'child_process'
 // var execSync = require('child_process').execSync;
 
-config();//.env file vars added to process.env
+config()// .env file vars added to process.env
+
+const COMPOSE_PROJECT_NAME = process.env.COMPOSE_PROJECT_NAME
+const POSTGRES_USER = process.env.POSTGRES_USER
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD
+const SUPER_USER = process.env.SUPER_USER
+const SUPER_USER_PASSWORD = process.env.SUPER_USER_PASSWORD
+
+const DB_HOST = process.env.DB_HOST
+const DB_NAME = process.env.DB_NAME
+const PG = `${COMPOSE_PROJECT_NAME}_db_1`
+
+const psql_version = spawnSync('psql', ['--version'])
+const have_psql = (psql_version.stdout && psql_version.stdout.toString('utf8').trim().length > 0)
+
+export function rest_service () {
+  return request(process.env.SERVER_PROXY_URI)
+}
+
+export function resetdb () {
+  let pg
+  if (have_psql) {
+    var env = Object.create(process.env)
+    env.PGPASSWORD = SUPER_USER_PASSWORD
+    pg = spawnSync('psql', ['-h', 'localhost', '-U', SUPER_USER, DB_NAME, '-f', process.env.PWD + '/db/src/sample_data/reset.sql'], { env: env })
+  } else {
+    pg = spawnSync('docker', ['exec', PG, 'psql', '-U', SUPER_USER, DB_NAME, '-f', 'docker-entrypoint-initdb.d/sample_data/reset.sql'])
+  }
+  if (pg.status !== 0) {
+    throw new Error(`Could not reset database in rest tests. Error = ${pg.stderr.toString()}`)
+  }
+}
 
 request.Test.prototype.withRole = function (role) {
   if (typeof role !== 'string') {
@@ -21,42 +52,4 @@ request.Test.prototype.withRole = function (role) {
   let jwt = jsonwebtoken.sign(payload, process.env.JWT_SECRET)
 
   return this.set('Authorization', `Bearer ${jwt}`)
-}
-
-const COMPOSE_PROJECT_NAME = process.env.COMPOSE_PROJECT_NAME;
-const POSTGRES_USER = process.env.POSTGRES_USER;
-const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
-const SUPER_USER = process.env.SUPER_USER;
-const SUPER_USER_PASSWORD = process.env.SUPER_USER_PASSWORD;
-
-const DB_HOST = process.env.DB_HOST;
-const DB_NAME = process.env.DB_NAME;
-const PG = `${COMPOSE_PROJECT_NAME}_db_1`
-
-const psql_version = spawnSync('psql', ['--version']);
-const have_psql = (psql_version.stdout && psql_version.stdout.toString('utf8').trim().length > 0)
-
-
-var rest_service = function() { 
-  return request(process.env.SERVER_PROXY_URI);
-}
-
-const resetdb = () => {
-  let pg;
-  if (have_psql){
-    var env = Object.create( process.env );
-    env.PGPASSWORD = SUPER_USER_PASSWORD
-    pg = spawnSync('psql', ['-h', 'localhost', '-U', SUPER_USER, DB_NAME, '-f', process.env.PWD + '/db/src/sample_data/reset.sql'], { env: env })
-  }
-  else{
-    pg = spawnSync('docker', ['exec', PG, 'psql', '-U', SUPER_USER, DB_NAME, '-f', 'docker-entrypoint-initdb.d/sample_data/reset.sql'])
-  }
-  if(pg.status !== 0){
-    throw new Error(`Could not reset database in rest tests. Error = ${pg.stderr.toString()}`);
-  }
-}
-
-module.exports = {
-  resetdb: resetdb,
-  rest_service: rest_service
 }
