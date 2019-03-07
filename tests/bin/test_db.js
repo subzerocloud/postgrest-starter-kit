e045a091a8f6179f6e0538b8968ef56c20959d94
Ruslan Talpa <ruslan.talpa@gmail.com>
2019-03-07 11:32:35 +0200

    alter db tests scripts to be compatible with any PG version

--- tests/bin/test_db.js
+++ tests/bin/test_db.js
@@ -1,6 +1,6 @@
 const spawn = require('child_process').spawn;
 require('dotenv').config();
-
+const PG_MAJOR_VERSION = process.env.PG_VERSION.replace(/\..*/,'');
 spawn('docker', [
     'run',
     '-i',
@@ -13,6 +13,6 @@ spawn('docker', [
     '-e', `HOST=${process.env.DB_HOST}`,
     '-e', `DATABASE=${process.env.DB_NAME}`,
     '-e', `USER=${process.env.SUPER_USER}`,
-    '-e', `PASSWORD=${process.env.SUPER_USER_PASSWORD}`, 'lren/pgtap',
+    '-e', `PASSWORD=${process.env.SUPER_USER_PASSWORD}`, `subzerocloud/pgtap:pg${PG_MAJOR_VERSION}`,
 ],
 { stdio: 'inherit' });
